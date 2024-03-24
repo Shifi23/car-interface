1bd60e4a65e37d085f7c6df8a00d5015a1c3947f
Shifi23 <63830738+Shifi23@users.noreply.github.com>
2024-03-24 07:24:33 +0000

    databasing

--- interface/controls/controls_api.py
+++ interface/controls/controls_api.py
@@ -3,15 +3,17 @@ from pydantic import BaseModel
 from interface.database import get_db
 from interface.models import Controls
 from sqlalchemy.orm import Session
-from typing import Annotated
+from typing import Annotated, List
+from datetime import datetime
 
 class ControlStatusesBase(BaseModel):
-    locked: bool
-    lights_on: bool
-    engine_on: bool
+    locked: bool = False
+    lights_on: bool = False
+    engine_on: bool = False
 
 class ControlStatuses(ControlStatusesBase):
     id: int
+    created_at: datetime
 
     class Config:
         from_attributes = True
@@ -19,34 +21,84 @@ class ControlStatuses(ControlStatusesBase):
 db_dependany = Annotated[Session, Depends(get_db)]
 router = APIRouter(tags=["Controls"])
 
-@router.get("/status")
-async def get_control_statuses():
-    return []
+def get_latest_iteration(db: db_dependany):
+    if not db.query(Controls).order_by(Controls.id.desc()).first():
+        db_record = Controls(**ControlStatusesBase().model_dump())
+        db.add(db_record)
+        db.commit()
+        db.refresh(db_record)
+        return db.query(Controls).order_by(Controls.id.desc()).first().__dict__
+    else:
+        return db.query(Controls).order_by(Controls.id.desc()).first().__dict__
+    
 
-@router.post("/lock", response_model=ControlStatuses)
-async def lock_car(status: ControlStatusesBase, db: db_dependany):
-    db_lock = Controls(ControlStatusesBase(locked=False, ))
-    db.add(db_lock)
+@router.get("/status", response_model=ControlStatuses)
+async def get_control_statuses(db: db_dependany):
+    statues = get_latest_iteration(db)
+    return statues
+
+@router.post("/lock")
+async def lock_car(db: db_dependany):
+    db_record = ControlStatusesBase(**db.query(Controls).order_by(Controls.id.desc()).first().__dict__).model_dump()
+    update_status = {"locked": True}
+    db_record |= update_status
+    db_record = Controls(**db_record)
+    db.add(db_record)
     db.commit()
-    db.refresh(db_lock)
-    return db_lock
+    db.refresh(db_record)
+    return 200
 
 @router.delete("/lock")
-async def unlock_car():
+async def unlock_car(db: db_dependany):
+    db_record = ControlStatusesBase(**db.query(Controls).order_by(Controls.id.desc()).first().__dict__).model_dump()
+    update_status = {"locked": False}
+    db_record |= update_status
+    db_record = Controls(**db_record)
+    db.add(db_record)
+    db.commit()
+    db.refresh(db_record)
     return 200
 
 @router.post("/lights")
-async def turn_on_lights():
+async def turn_on_lights(db: db_dependany):
+    db_record = ControlStatusesBase(**db.query(Controls).order_by(Controls.id.desc()).first().__dict__).model_dump()
+    update_status = {"lights_on": True}
+    db_record |= update_status
+    db_record = Controls(**db_record)
+    db.add(db_record)
+    db.commit()
+    db.refresh(db_record)
     return 200
 
 @router.delete("/lights")
-async def turn_off_lights():
+async def turn_off_lights(db: db_dependany):
+    db_record = ControlStatusesBase(**db.query(Controls).order_by(Controls.id.desc()).first().__dict__).model_dump()
+    update_status = {"lights_on": False}
+    db_record |= update_status
+    db_record = Controls(**db_record)
+    db.add(db_record)
+    db.commit()
+    db.refresh(db_record)
     return 200
 
 @router.post("/engine")
-async def turn_on_engine():
+async def turn_on_engine(db: db_dependany):
+    db_record = ControlStatusesBase(**db.query(Controls).order_by(Controls.id.desc()).first().__dict__).model_dump()
+    update_status = {"engine_on": True}
+    db_record |= update_status
+    db_record = Controls(**db_record)
+    db.add(db_record)
+    db.commit()
+    db.refresh(db_record)
     return 200
 
 @router.delete("/engine")
-async def turn_off_engine():
+async def turn_off_engine(db: db_dependany):
+    db_record = ControlStatusesBase(**db.query(Controls).order_by(Controls.id.desc()).first().__dict__).model_dump()
+    update_status = {"engine_on": False}
+    db_record |= update_status
+    db_record = Controls(**db_record)
+    db.add(db_record)
+    db.commit()
+    db.refresh(db_record)
     return 200
