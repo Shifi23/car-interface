f589ad4d18e10721c6111c54acb5571b6d76cbee
shifi23 <shuhrat.talukder@gmail.com>
2024-10-30 01:42:52 +0000

    versioning

--- backend/interface/main.py
+++ backend/interface/main.py
@@ -8,7 +8,7 @@ from backend.interface.database import engine
 import backend.interface.models
 
 backend.interface.models.Base.metadata.create_all(bind=engine)
-app = FastAPI(title="Car Interface APIs", version="0.1.9", docs_url="/")
+app = FastAPI(title="Car Interface APIs", version="0.2.0", docs_url="/")
 
 origins = [
     'http://localhost:3000'
@@ -24,7 +24,7 @@ app.add_middleware(
 
 @app.get("/version", tags=["Car-Interface"])
 async def get_car_interface_version():
-    return {"version": "0.1.9"}
+    return {"version": "0.2.0"}
 
 ## add routes here
 app.include_router(
