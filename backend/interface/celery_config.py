6bf7dd640c52f85220c18863de8fce28906aa236
shifi23 <shuhrat.talukder@gmail.com>
2024-11-01 06:28:13 +0000

    enough for today

--- backend/interface/celery_config.py
+++ backend/interface/celery_config.py
@@ -2,6 +2,17 @@ from celery.app import Celery
 
 celery_app = Celery(__name__, broker="redis://127.0.0.1:6379/0", backend="redis://127.0.0.1:6379/0")
 
+celery_app.conf.update(
+    task_serializer="json",
+    result_serializer="json",
+    accept_content=["json"],
+    timezone="UTC",
+    enable_utc=True,
+    broker_pool_limit=None,
+    broker_heartbeat=10,
+    broker_connection_retry_on_startup=True,
+    broker_connection_max_retries=None,
+)
 
 celery_app.autodiscover_tasks(
     [
